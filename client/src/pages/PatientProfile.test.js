87e18b6e68a7f864242dcf996f79c855f7d4d9fc
kasethi23 <119481188+kasethi23@users.noreply.github.com>
2025-02-24 10:35:44 -0500

    add in 1 more test case

--- client/src/pages/PatientProfile.test.js
+++ client/src/pages/PatientProfile.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import PatientProfile from "./PatientProfile"; 
 
-describe("PatientProfile - Password Visibility", () => {
+describe("PatientProfile Component", () => {
   test("toggles password visibility when visibility icon is clicked", () => {
     render(<PatientProfile />);
     const passwordInput = screen.getByLabelText("Password");
@@ -13,7 +13,23 @@ describe("PatientProfile - Password Visibility", () => {
     fireEvent.click(visibilityToggle);
     expect(passwordInput.getAttribute("type")).toBe("password");
   });
-});
 
+  test("displays error messages when trying to save with empty fields", () => {
+    render(<PatientProfile />);
+    
+
+    const updateButton = screen.getByRole("button", { name: /update information/i });
+    fireEvent.click(updateButton);
+
+    const inputFields = screen.getAllByRole("textbox");
+    inputFields.forEach((input) => fireEvent.change(input, { target: { value: "" } }));
+
+    const saveButton = screen.getByRole("button", { name: /save changes/i });
+    fireEvent.click(saveButton);
+
+    const errorMessages = screen.getAllByText("This field is required");
+    expect(errorMessages.length).toBeGreaterThan(0);
+  });
+});
 
 
