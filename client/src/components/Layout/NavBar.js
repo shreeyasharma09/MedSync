3372fed11bdfae9d3c93f93b4dfea5005905e630
VishnudevP <vipoil04@gmail.com>
2025-03-23 19:17:02 -0400

    Added User Roles for Navbar

--- client/src/components/Layout/NavBar.js
+++ client/src/components/Layout/NavBar.js
@@ -31,7 +31,10 @@ const NavLink = ({to, label}) => {
 const NavBar = () => {
   const navigate = useNavigate();
   const auth = getAuth();
+
   const isLoggedIn = !!auth.currentUser;
+
+  const userRole = localStorage.getItem('userRole');
 
   const handleLogout = async () => {
     try {
@@ -44,20 +47,42 @@ const NavBar = () => {
   };
 
   return (
-    <AppBar position="static" elevation={0} style={{backgroundColor: 'white'}}>
+    <AppBar
+      position="static"
+      elevation={0}
+      style={{backgroundColor: 'white', borderBottom: '1px solid #e0e0e0'}}
+    >
       <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
         <Typography variant="h6" style={{color: '#3e4b32', fontWeight: 'bold'}}>
           <span style={{color: '#708b69'}}>Med</span>Sync
         </Typography>
 
-        {!isLoggedIn && <Box>{<NavLink to="/Landing" label="Landing" />}</Box>}
-
-        {isLoggedIn && (
+        {!isLoggedIn && (
           <Box>
-            {/* <NavLink to="/Landing" label="Landing" /> */}
+            <NavLink to="/Landing" label="Landing" />
+          </Box>
+        )}
+
+        {isLoggedIn && userRole === 'patient' && (
+          <Box>
             <NavLink to="/PatientDashboard" label="Dashboard" />
             <NavLink to="/PatientBookings" label="Bookings" />
             <NavLink to="/profile/p" label="Profile" />
+            <Button
+              onClick={handleLogout}
+              color="inherit"
+              style={{color: 'black'}}
+            >
+              Logout
+            </Button>
+          </Box>
+        )}
+
+        {isLoggedIn && userRole === 'healthcare' && (
+          <Box>
+            <NavLink to="/HealthcareDashboard" label="Dashboard" />
+            <NavLink to="/HealthcareBookings" label="Bookings" />
+            <NavLink to="/profile/hp" label="Profile" />
             <Button
               onClick={handleLogout}
               color="inherit"
